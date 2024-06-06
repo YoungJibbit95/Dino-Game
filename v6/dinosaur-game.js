e8fd7837ffcc66b219ec0c2d9779a8e57fe534b7
young_jibbit95 <166993893+YoungJibbit95@users.noreply.github.com>
2024-06-06 20:58:11 +0200

    Add files via upload

---
+++ v6/dinosaur-game.js
@@ -0,0 +1,173 @@
+$(document).ready(function () {
+    let gameRunning = false;
+    let gameSpeed = 4;
+    let score = 0;
+    let dino = $('#dino');
+    let jumpInProgress = false;
+    let obstacleInterval;
+    let pause = false;
+
+    function openKeybindsMenu() {
+        $('#keybinds-menu').removeClass('hidden');
+    }
+
+    function closeKeybindsMenu() {
+        $('#keybinds-menu').addClass('hidden');
+    }
+
+    function startGame() {
+        gameRunning = true;
+        $('#start-button').hide();
+        $('#game-canvas').removeClass('hidden');
+        $('#menu').addClass('hidden');
+        $('#score').removeClass('hidden');
+        $('#score').text('Score: ' + score);
+
+        obstacleInterval = setInterval(function () {
+            if (gameRunning && !pause && $('.obstacle').length === 0) {
+                createObstacle();
+                score++;
+                $('#score').text('Score: ' + score);
+
+                if (score % 10 === 0) {
+                    gameSpeed++;
+                }
+            }
+        }, 1000);
+
+        $('#jump-button').on('click', function () {
+            if (!jumpInProgress && gameRunning && !pause) {
+                jump();
+            }
+        });
+
+        $(document).on('keydown', function (e) {
+            if (e.keyCode === 32 && !jumpInProgress && gameRunning && !pause) {
+                jump();
+            } else if (e.keyCode === 80 && gameRunning) {
+                pauseGame();
+            }
+        });
+
+        $(window).off('blur');
+    }
+
+    function jump() {
+        let jumpHeight = 260;
+        let jumpDistance = 30; 
+        let jumpTime = 1000;
+    
+        jumpInProgress = true;
+    
+        dino.animate({ 
+            bottom: "+=" + jumpHeight + "px",
+            left: "+=" + jumpDistance + "px"
+        }, jumpTime / 2, function () {
+            dino.animate({ 
+                bottom: "0px",
+                left: "-=" + jumpDistance + "px"
+            }, jumpTime / 2, function () {
+                jumpInProgress = false;
+            });
+        });
+    }
+    
+
+    function createObstacle() {
+        let obstacle = $('<div class="obstacle"></div>').appendTo('#game-canvas');
+        let obstaclePosition = $('#game-canvas').width();
+        let obstacleHeight = 65;
+        let obstacleWidth = 15;
+        obstacle.css({ 'left': obstaclePosition, 'height': obstacleHeight + 'px' });
+    
+        let obstacleMovement = setInterval(function () {
+            if (gameRunning && !pause) {
+                obstaclePosition -= gameSpeed;
+                obstacle.css('left', obstaclePosition);
+    
+                if (checkCollision(dino, obstacle)) {
+                    endGame();
+                }
+    
+                if (obstaclePosition < -300) {
+                    obstacle.remove();
+                    clearInterval(obstacleMovement);
+                }
+            }
+        }, 20);
+    }
+    
+
+    function checkCollision(dino, obstacle) {
+        
+        let dinoBox = dino[0].getBoundingClientRect();
+        let dinoLeft = dinoBox.left;
+        let dinoRight = dinoBox.right;
+        let dinoTop = dinoBox.top;
+        let dinoBottom = dinoBox.bottom;
+
+        let obstacleBox = obstacle[0].getBoundingClientRect();
+        let obstacleLeft = obstacleBox.left;
+        let obstacleRight = obstacleBox.right;
+        let obstacleTop = obstacleBox.top;
+        let obstacleBottom = obstacleBox.bottom;
+
+        return (
+            dinoRight > obstacleLeft &&
+            dinoLeft < obstacleRight &&
+            dinoBottom > obstacleTop &&
+            dinoTop < obstacleBottom
+        );
+    }
+
+    function endGame() {
+        gameRunning = false;
+        alert('Game Over! Your score: ' + score);
+        score = 0;
+        $('.obstacle').remove();
+        openMenu();
+    }
+
+    function openMenu() {
+        gameRunning = false;
+        $('#start-button').show();
+        $('#game-canvas').addClass('hidden');
+        $('#menu').removeClass('hidden');
+        $('#score').addClass('hidden');
+    }
+
+    function pauseGame() {
+        pause = !pause;
+
+        if (pause) {
+            clearInterval(obstacleInterval);
+        } else {
+            obstacleInterval = setInterval(function () {
+                if (gameRunning && !pause && $('.obstacle').length === 0) {
+                    createObstacle();
+                    score++;
+                    $('#score').text('Score: ' + score);
+
+                    if (score % 10 === 0) {
+                        gameSpeed++;
+                    }
+                }
+            }, 1000);
+        }
+    }
+
+    $('#start-button').on('click', startGame);
+    $('#settings-button').on('click', openMenu);
+    $('#close-menu-button').on('click', function () {
+        $('#menu').addClass('hidden');
+    });
+    $('#speed-slider').on('input', function () {
+        gameSpeed = $('#speed-slider').val();
+    });
+    $('#theme-toggle').on('click', function () {
+        $('body').toggleClass('dark-theme');
+    });
+
+    $('#keybinds-button').on('click', openKeybindsMenu);
+    $('#close-keybinds-button').on('click', closeKeybindsMenu);
+});
